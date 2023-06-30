1c115492d39a9e88968ccb3a19a77b375ea712e8
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-06-30 22:02:31 +0530

    feat: add shadcn ui & implement toggle theme

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ModeToggle from './ThemeToggle';
+
+const Navbar = () => {
+  return (
+    <div>
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default Navbar;
