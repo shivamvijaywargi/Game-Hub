da39f3ae10428eeb676e951183f37c1cd7b758d0
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-07-02 17:15:21 +0530

    wip: wip navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
+
 import ModeToggle from './ThemeToggle';
+import Logo from '@/assets/logo.webp';
 
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center">
+      <div className="flex items-center">
+        <Image src={Logo} alt="Game Hub" className="w-14 h-14" />
+        <h3 className="text-lg">Game Hub</h3>
+      </div>
+
       <ModeToggle />
     </div>
   );
