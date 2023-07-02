e917b6c215fbdeb69014f65d50f81ca55f11ea77
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-07-02 16:43:39 +0530

    feat: build responsive home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <main className="">Hello Game Hub</main>;
+  return (
+    <main className="grid grid-cols-1 lg:grid-cols-2">
+      <aside className="hidden lg:block">Aside</aside>
+      <div className="">main</div>
+    </main>
+  );
 }
