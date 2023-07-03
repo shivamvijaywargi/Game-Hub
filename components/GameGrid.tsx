5ed6a0dd25fdb2d22ee7b1ae590d8a86c8369b70
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-07-03 10:43:35 +0530

    ui: expand grid on 2xl screen

--- components/GameGrid.tsx
+++ components/GameGrid.tsx
@@ -8,7 +8,7 @@ const GameGrid = async () => {
   const gameData = await getGames();
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
       {gameData?.map((game) => (
         <div key={game.id} className="self-stretch">
           <GameCard game={game} />
